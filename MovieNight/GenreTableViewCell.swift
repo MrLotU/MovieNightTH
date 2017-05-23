7f54213836c36de2442382838641cca7f3ceb906
MrLotU <j.koopman@jarict.nl>
2017-05-23 11:14:01 +0200

    Started on UI

---
+++ MovieNight/GenreTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GenreTableViewCell.swift
+//  MovieNight
+//
+//  Created by Jari Koopman on 23/05/2017.
+//  Copyright © 2017 JarICT. All rights reserved.
+//
+
+import UIKit
+
+class GenreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MovieNight/MovieTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieTableViewCell.swift
+//  MovieNight
+//
+//  Created by Jari Koopman on 23/05/2017.
+//  Copyright © 2017 JarICT. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
